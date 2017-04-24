cfecf8f46cc63614d5c9e98bd48c6853f23de42d
Cy Kong <cykong@Regie-Kongs-MacBook-Pro.local>
2017-04-23 20:01:48 -0700

    detect form functioning on paypal

--- challenge.js
+++ challenge.js
@@ -7,19 +7,72 @@
  * Feel free to modify these.
  */
 function getUsernameField() {
-  return $("#email");
+  var email
+  //for inputs 
+  var inputs = document.getElementsByTagName('input');
+
+    for(var i = 0; i < inputs.length; i++) {
+      if(inputs[i].type.toLowerCase() == 'email' || inputs[i].id.toLowerCase() == 'email'  || inputs[i].id.toLowerCase() == 'username'|| inputs[i].class == 'email' || inputs[i].name.toLowerCase() == 'email') {
+        email = inputs[i];
+      }
+    }
+    console.log('got username');
+    return email;
+    
+
+  //for select fields
+
+  // var select = document.getElementsByTagName('select');
+  // if(select) {
+  //   console.log('no inputs, selecting')
+  //   console.log('select', select);
+  //   email = select;
+  // }
+
+
+  // return email;
+  // console.log($("#email"))
+  // return $("#email");
 }
 
 function getPasswordField() {
-  return $("#pass");
+  var password
+
+  var inputs = document.getElementsByTagName('input');
+
+  for(var i = 0; i < inputs.length; i++) {
+    console.log('type of class', typeof inputs[i].id);
+    if(inputs[i].type.toLowerCase().includes('pass') || inputs[i].id.toLowerCase().includes('pass') || inputs[i].name.includes('pass')) {
+      console.log('input', i, inputs[i]);
+      password = inputs[i];
+      console.log('password', password)
+      break;
+    }
+  }
+  console.log('got password');
+  return password;
+//return $("#pass");
 }
 
 function getFormField() {
-  return getUsernameField().closest('form');
+  return getPasswordField().closest('form');
 }
 
 function getSubmitButton() {
-  return $("#loginbutton");
+  var submit;
+  var login = document.getElementsByTagName('input');
+console.log('login array', login)
+  for(var i = 0; i < login.length; i++) {
+     if(login[i].id.toLowerCase().includes('sign') || login[i].id.toLowerCase().includes('log') || login[i].value.toLowerCase().includes('log') || login[i].name.toLowerCase().includes('log')) {
+      console.log('submit', i, login[i]);
+      submit = login[i];
+      console.log('submit', submit);
+      break;
+    }
+  }
+  console.log('got submit button');
+  return submit;
+  //return $("#loginbutton");
 }
 
 /**
